96cd94c85b919d9fda24bf7757f92c1f11574575
@KirubakaranSelvam <kirubakaran.s@finstein.ai>
2024-01-22 18:13:31 +0530

    loader

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,38 @@ import {  HttpClientModule } from '@angular/common/http';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxUiLoaderConfig, NgxUiLoaderHttpModule, NgxUiLoaderModule } from 'ngx-ui-loader';
+
+const ngx: NgxUiLoaderConfig = {
+  "bgsColor": "rgb(35,25,66)",
+  "bgsOpacity": 0.5,
+  "bgsPosition": "bottom-right",
+  "bgsSize": 60,
+  "bgsType": "ball-spin-clockwise",
+  "blur": 1000,
+  "delay": 0,
+  "fastFadeOut": false,
+  "fgsColor": "rgb(35,25,66)",
+  "fgsPosition": "center-center",
+  "fgsSize": 80,
+  "fgsType": "rectangle-bounce-pulse-out-rapid",
+  "gap": 24,
+  "logoPosition": "center-center",
+  "logoSize": 90,
+  // "logoUrl": "assets/m.jpg",
+  "masterLoaderId": "master",
+  "overlayBorderRadius": "0",
+  "overlayColor": "rgba(40, 40, 40, 0.8)",
+  "pbColor": "#4285F4",
+  "pbDirection": "ltr",
+  "pbThickness": 3,
+  "hasProgressBar": true,
+  "text": "",
+  "textColor": "#FFFFFF",
+  "textPosition": "center-center",
+  "maxTime": 0,
+  "minTime": 0
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,7 +59,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ButtonModule,
     HttpClientModule,
     ToastModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxUiLoaderHttpModule.forRoot({ showForeground: true }),
+    NgxUiLoaderModule.forRoot(ngx),
     
   ],
   providers: [ApiService,MessageService],
